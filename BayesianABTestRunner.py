a9213ef3c1b23f0fa4f21bb7cbe262742fb63328
Alberto Boschetti <dreambosko@gmail.com>
2017-04-04 01:06:56 +0200

    bandits

--- BayesianABTestRunner.py
+++ BayesianABTestRunner.py
@@ -23,33 +23,31 @@ class BayesianABTestRunner(object):
         if name == "bandits":
             self.test = bandits
 
-
-    def run_test(self, epsilon, data_samples, n_steps):
+    def run_test(self, data_samples, n_steps, epsilon=None):
         """Run the test
 
         Args:
-            epsilon (Double): The exploration epsilon
             data_samples(List[numpy.array]): each element of the list is an array of observations
             n_steps: the number of steps to run
+            epsilon (Double): The exploration epsilon (if needed by the algo)
         """
         assert isinstance(data_samples, list)
         for el in data_samples:
             assert type(el) == np.ndarray
 
-        self.test(epsilon, data_samples, n_steps)
+        assert n_steps > 0
+        for group in data_samples:
+            assert issubclass(group.dtype.type, np.integer)
+            assert set(np.unique(group)) == set((0, 1))
+
+        self.test(data_samples, n_steps, epsilon)
 
 
-
-def ucb1(epsilon, data_samples, n_steps):
+def ucb1(data_samples, n_steps, epsilon):
     """
     Epsilon greedy, or UCB1 algorithm
     
     """
-    assert n_steps > 0
-    for group in data:
-        assert issubclass(group.dtype.type, np.integer)
-        assert set(np.unique(group)) == set((0, 1))
-
     n_groups = len(data_samples)
     punctual_means = np.zeros((n_groups, ))
     punctual_extractions = [list([]) for _ in range(n_groups)]
@@ -68,10 +66,8 @@ def ucb1(epsilon, data_samples, n_steps):
             # Exploit
             arm = np.argmax(punctual_means)
 
-
         extraction = (data_samples[arm])[idxs[arm]]
         idxs[arm] += 1
-
 
         extractions.append(extraction)
         punctual_extractions[arm].append(extraction)
@@ -83,17 +79,15 @@ def ucb1(epsilon, data_samples, n_steps):
                   "arm performance", punctual_means)
 
 
-def bandits(epsilon, data_samples, n_steps):
+def bandits(data_samples, n_steps, *args):
     """
     Multi arm bandits
 
     """
-    assert n_steps > 0
-    for group in data:
-        assert issubclass(group.dtype.type, np.integer)
-        assert set(np.unique(group)) == set((0, 1))
-
     n_groups = len(data_samples)
+
+    alpha_beta_params = [{'a': 1.0, 'b': 1.0} for _ in range(n_groups)]
+
     punctual_means = np.zeros((n_groups,))
     punctual_extractions = [list([]) for _ in range(n_groups)]
     idxs = np.zeros((n_groups,), dtype=int)
@@ -101,7 +95,24 @@ def bandits(epsilon, data_samples, n_steps):
     extractions = []
 
     for i in range(n_steps):
-        pass
+        samples = [np.random.beta(bandit['a'], bandit['b']) for bandit in alpha_beta_params]
+        arm = np.argmax(samples)
+
+        extraction = (data_samples[arm])[idxs[arm]]
+        idxs[arm] += 1
+
+        alpha_beta_params[arm]['a'] += extraction
+        alpha_beta_params[arm]['b'] += 1 - extraction
+
+        extractions.append(extraction)
+        punctual_extractions[arm].append(extraction)
+        punctual_means[arm] = np.mean(punctual_extractions[arm])
+
+        if i % 100 == 0:
+            print("step", i,
+                  "global mean", np.mean(extractions),
+                  "arm performance", punctual_means)
+
 
 if __name__ == "__main__":
     from GenerateSamples import GenerateBinarySamples, BinaryGroupParams
@@ -117,7 +128,4 @@ if __name__ == "__main__":
     tests.run_test(epsilon=0.05, data_samples=data, n_steps=10001)
 
     tests = BayesianABTestRunner("bandits")
-    tests.run_test(epsilon=0.05, data_samples=data, n_steps=10001)
-
-
-
+    tests.run_test(epsilon=0.01, data_samples=data, n_steps=10001)
